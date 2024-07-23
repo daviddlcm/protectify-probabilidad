fc289efdc6362a410e3d340174f63a5d35658db8
david <daviddelacruzmorales42@gmail.com>
2024-07-23 09:14:06 -0600

    update

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ dotenv.config();
 
 const app = express();
 app.use(express.static('build'));
-app.use(cors());
+app.use(cors({
+    origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 const conexion = mysql.createPool({
     host: process.env.DB_HOST,
